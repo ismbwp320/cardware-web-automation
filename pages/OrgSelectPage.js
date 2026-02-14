7ef334f6a99bfcb5fe9d0c591bc5fd1c15a4add7
ismbwp320 <ismbwp320@gmail.com>
2026-02-14 22:17:25 +0500

    base setup

--- pages/OrgSelectPage.js
+++ pages/OrgSelectPage.js
@@ -20,9 +20,7 @@ export class OrgSelectPage {
   }
 
   async changeOrganization( orgName) {
-    // Wait for the org selector area to be visible
-    await expect(this.locationOption()).toBeVisible();
-
+    await this.page.waitForTimeout(5000);
     await this.locationOption().click();
 
     await expect(this.orgHeading(orgName)).toBeVisible();
@@ -32,41 +30,3 @@ export class OrgSelectPage {
     await this.page.waitForLoadState("networkidle");
   }
 }
-
-
-
-
-// import { expect } from "@playwright/test";
-// import { CatalystConfig } from "../config";
-// import path from "path";
-// import fs from "fs";
-
-// const authFile = path.join(__dirname, `../${CatalystConfig.AUTH_FILE}`);
-// const orgFile = path.join(__dirname, `../${CatalystConfig.ORG_FILE}`);
-
-// export class OrgSelectPage {
-//   constructor(page) {
-//     this.page = page;
-//   }
-
-//   async changeOrganization() {
-//     await this.page.context().storageState({ path: authFile });
-//       const storageState = JSON.parse(fs.readFileSync(authFile, "utf-8"));
-//       const state = storageState?.origins;
-//       const baseURL = state.length > 0 && state[0].origin;
-
-//     await this.page.goto(`${baseURL}/home`);
-
-//     await this.page.waitForTimeout(7000);
-//     await this.page
-//       .locator("div")
-//       .filter({ hasText: /^St\. Louis, MO$/ })
-//       .nth(3)
-//       .click();
-//     await this.page.waitForTimeout(2000);
-//     await this.page.getByRole("heading", { name: "720 Hub Dev" }).click();
-//     await this.page.waitForTimeout(2000);
-//     await this.page.context().storageState({ path: orgFile });
-//     await this.page.waitForTimeout(5000);
-//   }
-// }
--- tests/base.spec.js
+++ tests/base.spec.js
@@ -3,54 +3,5 @@ import { test, expect } from "@playwright/test";
 test("Home page opens (already logged in + org selected)", async ({ page }) => {
   await page.goto("/home");
   await expect(page).toHaveURL(/home/i);
+  await page.waitForTimeout(9000);
 });
-
-
-
-
-
-// import { test as base } from '@playwright/test';
-// import { LoginPage } from '../pages/LoginPage.js';
-// import { OrgSelectPage } from '../pages/OrgSelectPage.js';
-// // Extend the base test to make all tests run serially
-// const test = base.extend({});
-
-// test.describe.configure({ mode: 'serial' }); // <-- This forces serial execution
-
-// test.describe('LMA Web Application', () => {
-//   // Declare variables for pages and components
-//   let page, login, org, context ;
-
-//   // ---------------------- SETUP ----------------------
-//   test.beforeAll(async ({ browser }) => {
-//     // Create a new browser context and page instance
-//     context = await browser.newContext();
-//     page = await context.newPage();
-
-//     // Initialize all page objects
-//     login = new LoginPage(page);
-//     org = new OrgSelectPage(page);
-//   });
-
-//   // ---------------------- TEST CASE #1 ----------------------
-//   test('Login Flow', async () => {
-
-//     // Step 1: Navigate to the application login page
-//     await login.goto();
-
-//     // Step 2: Perform user login
-//     if (!process.env.USERNAME || !process.env.PASSWORD) {
-//     throw new Error("USERNAME or PASSWORD is missing in environment variables");
-//   }     
-//     await login.login(process.env.USERNAME, process.env.PASSWORD);
-//   });
-
-//   // ---------------------- TEST CASE #2 ----------------------
-//   test('Home Page Flow', async () => {
-//     await org.changeOrganization();
-//   });
-
-//   test.afterAll(async () => {
-//     await context.close();
-//   });
-// });
